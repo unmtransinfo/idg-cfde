efb0f36248a50fe81ec87033997aa6b8674fe495
Jeremy Yang <jeremyjyang@gmail.com>
2021-01-15 11:30:02 -0700

    Ok.

--- sh/Go_targetPages.sh
+++ sh/Go_targetPages.sh
@@ -28,6 +28,23 @@ printf "N_targets = %d\n" "$N"
 ID_NAMESPACE="http://nih-cfde.org/idg/tcrd"
 TCRD_VERSION="670"
 
+if [ "$(which sha256sum)" ]; then
+	SHA_EXE="sha256sum"
+elif [ "$(which sha)" ]; then
+	SHA_EXE="sha -a 256"
+else
+	echo "ERROR: Cannot find SHA_EXE."
+	exit
+fi
+if [ "$(which md5sum)" ]; then
+	MD5_EXE="md5sum"
+elif [ "$(which md5)" ]; then
+	MD5_EXE="md5"
+else
+	echo "ERROR: Cannot find MD5_EXE."
+	exit
+fi
+
 I=0
 while [ $I -lt $N ]; do
 	I=$(($I + 1))
@@ -39,8 +56,8 @@ while [ $I -lt $N ]; do
 	LOCAL_ID="TARGET_ID_${TID}"
 	PERSISTENT_ID="${ID_NAMESPACE}/${TCRD_VERSION}/${LOCAL_ID}"
 	SIZE_IN_BYTES=$(cat $FILENAME |wc -c)
-	SHA256=$(cat $FILENAME |sha -a 256 |sed 's/ .*$//')
-	MD5=$(cat $FILENAME |md5)
+	SHA256=$(cat $FILENAME |$SHA_EXE |sed 's/ .*$//')
+	MD5=$(cat $FILENAME |$MD5_EXE |sed 's/ .*$//')
 	printf "${ID_NAMESPACE}\t${LOCAL_ID}\t${PERSISTENT_ID}\t${SIZE_IN_BYTES}\t${SHA256}\t${MD5}\t${FILENAME}\n" \
 		>>$metadatafile
 done
