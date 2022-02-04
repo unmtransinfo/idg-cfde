5436b57044d3b6a28a7bd1e8daa001682e280940
Jeremy Yang <jeremyjyang@gmail.com>
2022-02-04 11:37:37 -0700

    Ok.

--- CFChem/sh/Go_cfchem_DbAddProperties.sh
+++ CFChem/sh/Go_cfchem_DbAddProperties.sh
@@ -11,7 +11,6 @@ DBHOST="localhost"
 #
 TNAME="properties"
 #
-cwd=$(pwd)
 TMPDIR="$(cd $HOME/../data/CFDE/data/; pwd)/tmp/$(date +'%Y-%m-%d')"
 printf "TMPDIR: ${TMPDIR}\n"
 if [ ! -e $TMPDIR ]; then
@@ -20,7 +19,7 @@ fi
 #
 ###
 # SMILES from molecules table:
-psql -d $DBNAME -c "COPY (SELECT cansmi, id FROM mols ORDER BY id) TO STDOUT WITH (FORMAT CSV,HEADER,DELIMITER E'\t')" \
+psql -e -d $DBNAME -c "COPY (SELECT cansmi, id FROM mols ORDER BY id) TO STDOUT WITH (FORMAT CSV,HEADER,DELIMITER E'\t')" \
 	>${TMPDIR}/${DBNAME}_mols.smi
 #
 ###
@@ -33,10 +32,64 @@ if [ ! "$CONDA_EXE" -o ! -e "$CONDA_EXE" ]; then
 fi
 #
 ###
-psql -d $DBNAME -c "DROP TABLE IF EXISTS $TNAME"
-psql -d $DBNAME -c "CREATE TABLE $TNAME AS SELECT id FROM mols"
+psql -e -d $DBNAME -c "DROP TABLE IF EXISTS $TNAME"
+psql -e -d $DBNAME -c "CREATE TABLE $TNAME AS SELECT id FROM mols"
+#
+#####################################################################
+# Descriptors:
+propfile=${TMPDIR}/${DBNAME}_mols_descriptors.smi
+#
+if [ ! -e "$propfile" ]; then
+	source $(dirname $CONDA_EXE)/../bin/activate rdktools
+	python3 -m rdktools.properties.App descriptors \
+		--i ${TMPDIR}/${DBNAME}_mols.smi \
+		--smilesColumn 0 --nameColumn 1 \
+		--o ${propfile} \
+		--iheader --oheader
+	conda deactivate
+	printf "Descriptors file generated: ${propfile}\n"
+else
+	printf "Descriptors file exists: ${propfile}\n"
+fi
 #
 ###
+colnames=$(cat ${propfile} |head -1 |sed 's/\([a-z]\)\([A-Z]\)/\1_\2/g'|tr '[:upper:]' '[:lower:]')
+printf "colnames: ${colnames}\n"
+propnames=$(echo $colnames |sed 's/smiles\s//' |sed 's/name\s//')
+N_props=$(echo $propnames |wc -w)
+printf "propnames ($N_props): ${propnames}\n"
+for propname in $propnames ; do
+	psql -e -d $DBNAME -c "ALTER TABLE $TNAME ADD COLUMN $propname VARCHAR(32)"
+done
+#
+N=$[$(cat ${propfile} |wc -l)-1]
+i=0
+while [ $i -lt $N ]; do
+        i=$[$i + 1]
+        line=$(cat ${propfile} |sed '1d' |sed "${i}q;d")
+        mol_id=$(echo "$line" |awk -F '\t' '{print $2}')
+	vals=$(echo $line |awk '{$1=$2=""; print $0}')
+	vals=$(echo $vals |sed "s/\s/', '/g" |sed "s/^\(.*\)$/'\1'/")
+	printf "${i}/${N}. mol_id=${mol_id} (${N_props} properties added)\n"
+	psql -d $DBNAME -c "UPDATE $TNAME SET ($(echo $propnames |sed 's/\s/, /g')) = ROW(${vals}) WHERE id = ${mol_id}"
+done
+#
+#
+for colname in $propnames ; do
+	if [ "$(echo $colname |grep 'count')" -o "$(echo $colname |grep '^num_')" ]; then
+		psql -e -d $DBNAME -c "ALTER TABLE $TNAME ALTER COLUMN $colname TYPE INT USING $colname::INTEGER"
+		printf "Column ${TNAME}.${colname} altered to INT.\n"
+	elif [ "$(echo $colname |grep 'mol_wt')" -o "$(echo $colname |grep 'partial_charge')" ]; then
+		psql -e -d $DBNAME -c "ALTER TABLE $TNAME ALTER COLUMN $colname TYPE FLOAT USING $colname::FLOAT"
+		printf "Column ${TNAME}.${colname} altered to FLOAT.\n"
+	else
+		printf "Column ${TNAME}.${colname} unaltered.\n"
+	fi
+done
+#
+#
+#
+#####################################################################
 # Lipinski properties:
 propfile=${TMPDIR}/${DBNAME}_mols_lipinski_properties.smi
 #
@@ -60,7 +113,7 @@ propnames=$(echo $colnames |sed 's/smiles\s//' |sed 's/name\s//')
 N_props=$(echo $propnames |wc -w)
 printf "propnames ($N_props): ${propnames}\n"
 for propname in $propnames ; do
-	psql -d $DBNAME -c "ALTER TABLE $TNAME ADD COLUMN $propname VARCHAR(32)"
+	psql -e -d $DBNAME -c "ALTER TABLE $TNAME ADD COLUMN $propname VARCHAR(32)"
 done
 #
 N=$[$(cat ${propfile} |wc -l)-1]
@@ -88,9 +141,8 @@ for colname in $propnames ; do
 	fi
 done
 #
-###
+#####################################################################
 # LogP
-# SMILES 	Name 	WildmanCrippenLogP
 propfile=${TMPDIR}/${DBNAME}_mols_logp.smi
 if [ ! -e "$logpfile" ]; then
 	source $(dirname $CONDA_EXE)/../bin/activate rdktools
@@ -112,7 +164,7 @@ propnames=$(echo $colnames |sed 's/smiles\s//' |sed 's/name\s//')
 N_props=$(echo $propnames |wc -w)
 printf "propnames ($N_props): ${propnames}\n"
 for propname in $propnames ; do
-	psql -d $DBNAME -c "ALTER TABLE $TNAME ADD COLUMN $propname VARCHAR(32)"
+	psql -e -d $DBNAME -c "ALTER TABLE $TNAME ADD COLUMN $propname VARCHAR(32)"
 done
 #
 N=$[$(cat ${propfile} |wc -l)-1]
