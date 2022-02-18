3f8227c7626d99b1eeecdb608bff23b1d4f2295c
Jeremy Yang <jeremyjyang@gmail.com>
2022-02-18 12:23:01 -0700

    Alright.

--- sh/Go_diseasepages_Submit.sh
+++ sh/Go_diseasepages_Submit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ###
 # https://docs.nih-cfde.org/en/latest/cfde-submit/docs/
+# https://github.com/nih-cfde/published-documentation/wiki (table descriptions)
+# https://osf.io/rdeks/ (sample, empty table TSVs)
 ###
 set -e
-#set -x
 #
 ###
 # From https://docs.nih-cfde.org/en/latest/cfde-submit/docs/install/:
@@ -50,8 +51,6 @@ fi
 ${cwd}/sh/Go_c2m2_DownloadSampleTables.sh $DATAPATH
 #
 ###
-metadatafile="${DATADIR}/tcrd_diseasepages_c2m2.tsv"
-printf "id_namespace\tlocal_id\tproject_id_namespace\tproject_local_id\tpersistent_id\tcreation_time\tsize_in_bytes\tuncompressed_size_in_bytes\tsha256\tmd5\tfilename\tfile_format\tdata_type\tassay_type\tmime_type\n" >$metadatafile
 #
 ###
 ID_NAMESPACE="cfde_idg_tcrd"
@@ -76,6 +75,35 @@ else
 fi
 #
 #
+###
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-file.tsv
+# https://osf.io/qjeb5/
+echo "CREATE file.tsv (overwrite sample)."
+###
+printf "id_namespace\tlocal_id\tproject_id_namespace\tproject_local_id\tpersistent_id\tcreation_time\tsize_in_bytes\tuncompressed_size_in_bytes\tsha256\tmd5\tfilename\tfile_format\tdata_type\tassay_type\tmime_type\n" >${DATAPATH}/file.tsv
+###
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-collection.tsv
+# https://osf.io/3v2dt/
+# Currently one file per collection this datapackage.
+echo "CREATE collection.tsv (overwrite sample)."
+printf "id_namespace\tlocal_id\tpersistent_id\tcreation_time\tabbreviation\tname\tdescription\n" >${DATAPATH}/collection.tsv
+###
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-file_in_collection.tsv
+# https://osf.io/84jfy/
+# Currently one file per collection this datapackage.
+echo "CREATE file_in_collection.tsv (overwrite sample)."
+printf "file_id_namespace\tfile_local_id\tcollection_id_namespace\tcollection_local_id\n" >${DATAPATH}/file_in_collection.tsv
+###
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-disease.tsv
+# Currently one disease per file this datapackage.
+echo "CREATE disease.tsv (overwrite sample)."
+printf "id\tname\tdescription\tsynonyms\n" >${DATAPATH}/disease.tsv
+###
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-collection_disease.tsv
+# https://osf.io/r4uwa/
+# Currently one collection per disease this datapackage.
+echo "CREATE collection_disease.tsv (overwrite sample)."
+printf "collection_id_namespace\tcollection_local_id\tdisease\n" >${DATAPATH}/collection_disease.tsv
 #
 PROJECT_ID_NAMESPACE="cfde_idg_tcrd"
 PROJECT_LOCAL_ID="idgtcrd"
@@ -99,51 +127,52 @@ for ofile in $(ls $DATADIR/tcrd_disease_*.json) ; do
         UNCOMPRESSED_SIZE_IN_BYTES=${SIZE_IN_BYTES}
         SHA256=$(cat $ofile |$SHA_EXE |sed 's/ .*$//')
         MD5=$(cat $ofile |$MD5_EXE |sed 's/ .*$//')
-        printf "${ID_NAMESPACE}\t${LOCAL_ID}\t${PROJECT_ID_NAMESPACE}\t${PROJECT_LOCAL_ID}\t${PERSISTENT_ID}\t${CREATION_TIME}\t${SIZE_IN_BYTES}\t${UNCOMPRESSED_SIZE_IN_BYTES}\t${SHA256}\t${MD5}\t${FILENAME}\t${FILE_FORMAT}\t${DATA_TYPE}\t${ASSAY_TYPE}\t${MIME_TYPE}\n" >>$metadatafile
+        printf "${ID_NAMESPACE}\t${LOCAL_ID}\t${PROJECT_ID_NAMESPACE}\t${PROJECT_LOCAL_ID}\t${PERSISTENT_ID}\t${CREATION_TIME}\t${SIZE_IN_BYTES}\t${UNCOMPRESSED_SIZE_IN_BYTES}\t${SHA256}\t${MD5}\t${FILENAME}\t${FILE_FORMAT}\t${DATA_TYPE}\t${ASSAY_TYPE}\t${MIME_TYPE}\n" >>${DATAPATH}/file.tsv
+	DISEASE_NAME=$(cat $ofile |grep diseaseName |sed 's/^.*: "\(.*\)",/\1/')
+	COLLECTION_ABBREVIATION="${FILENAME}_collection"
+	COLLECTION_NAME="DiseasePage Collection: DOID:${DOID}"
+	COLLECTION_DESCRIPTION="DiseasePage Collection: ${DISEASE_NAME} (DOID:${DOID}, FILE=${FILENAME})"
+        printf "${ID_NAMESPACE}\t${LOCAL_ID}\t${PERSISTENT_ID}\t${CREATION_TIME}\t${COLLECTION_ABBREVIATION}\t${COLLECTION_NAME}\t${COLLECTION_DESCRIPTION}\n" >>${DATAPATH}/collection.tsv
 done
 #
+exit #DEBUG
 ###
-# CREATE file.tsv (overwrite sample).
-###
-cp $metadatafile ${DATAPATH}/file.tsv
-#
-###
-# CREATE id_namespace.tsv (overwrite sample).
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-id_namespace.tsv
+# https://osf.io/6gahk/
+echo "CREATE id_namespace.tsv (overwrite sample)."
 ###
 printf "id\tabbreviation\tname\tdescription\n" >${DATAPATH}/id_namespace.tsv
 printf "${PROJECT_ID_NAMESPACE}\tIDGTCRD\tIDG TCRD\tIDG Target Central Resource Database\n" >>${DATAPATH}/id_namespace.tsv
 ###
-# CREATE dcc.tsv (overwrite sample).
-###
-echo "*** TO DO ***: CREATE dcc.tsv"
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-dcc.tsv
+# https://osf.io/uvw9a/
+echo "CREATE dcc.tsv (overwrite sample)"
 ###
 # "contact_email\tcontact_name\tproject_id_namespace\tproject_local_id\tdcc_abbreviation\tdcc_name\tdcc_description\tdcc_url\n"
 printf "id\tdcc_name\tdcc_abbreviation\tdcc_description\tcontact_email\tcontact_name\tdcc_url\tproject_id_namespace\tproject_local_id\n" >${DATAPATH}/dcc.tsv
 printf "idg\tIlluminating the Druggable Genome (IDG)\tIDG\tThe goal of the Illuminating the Druggable Genome (IDG) program is to improve our understanding of the properties and functions of proteins that are currently unannotated within the three most commonly drug-targeted protein families: G-protein coupled receptors, ion channels, and protein kinases.\tjjyang@salud.unm.edu\tJeremy Yang\thttps://druggablegenome.net/\t${PROJECT_ID_NAMESPACE}\t${PROJECT_LOCAL_ID}\n" >>${DATAPATH}/dcc.tsv
 ###
-# CREATE project.tsv (overwrite sample).
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-project.tsv
+# https://osf.io/ns4zf/
+echo "CREATE project.tsv (overwrite sample)."
 ###
 printf "id_namespace\tlocal_id\tpersistent_id\tcreation_time\tabbreviation\tname\tdescription\n" >${DATAPATH}/project.tsv
 printf "${PROJECT_ID_NAMESPACE}\t${PROJECT_LOCAL_ID}\tidg_diseasepages\t${CREATION_TIME}\ttgtpgs\tidg_diseasepages\tIDG
 TCRD disease pages\n" >>${DATAPATH}/project.tsv
 ###
 ###
-# CREATE file_format.tsv (overwrite sample).
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-file_format.tsv
+# https://osf.io/9yzck/
+echo "CREATE file_format.tsv (overwrite sample)."
 ###
 printf "id\tname\tdescription\n" >${DATAPATH}/file_format.tsv
 printf "${FILE_FORMAT}\tJSON\tJavaScript Object Notation\n" >>${DATAPATH}/file_format.tsv
 ###
-# CREATE file_in_collection.tsv (overwrite sample).
-echo "*** TO DO ***: CREATE file_in_collection.tsv"
-###
-# CREATE disease.tsv (overwrite sample).
-echo "*** TO DO ***: CREATE disease.tsv"
-###
-# CREATE collection_disease.tsv (overwrite sample).
-echo "*** TO DO ***: CREATE collection_disease.tsv"
-###
-# CREATE collection_defined_by_project.tsv (overwrite sample).
-echo "*** TO DO ***: CREATE collection_defined_by_project.tsv"
+# https://github.com/nih-cfde/published-documentation/wiki/TableInfo:-collection_defined_by_project.tsv
+# https://osf.io/724sj/
+echo "CREATE collection_defined_by_project.tsv (overwrite sample)."
+# *** TO DO ***
+
 ###
 #cfde-submit run --help
 #
