4b272f3e727915263064f11010d2e631a2a320d4
Jeremy Yang <jeremyjyang@gmail.com>
2022-03-16 16:27:03 -0600

    Alright.

---
+++ Biomarkers/Go_loinc_GetData.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+###
+DBNAME="loinc"
+DBHOST="localhost"
+
+cwd="$(pwd)"
+
+DATADIR="${cwd}/data"
+
+sql="\
+SELECT
+	loinc_num,
+	component,
+	class,
+	definitiondescription,
+	status,
+	relatednames2,
+	shortname,
+	long_common_name,
+	displayname,
+	consumer_name
+FROM
+	loinc
+WHERE
+	class = 'CHEM'
+ORDER BY component, loinc_num
+"
+#
+psql -P pager=off -qAtF $'\t' -h $DBHOST -d $DBNAME -c "${sql}" \
+	>$DATADIR/loinc_chem_names.tsv
+#
+# Split relatednames2 into separate rows.
