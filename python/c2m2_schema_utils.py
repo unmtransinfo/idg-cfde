d4b27077416d0f8630ba14aa39858020cf6e5e38
Jeremy Yang <jeremyjyang@gmail.com>
2022-04-06 15:54:55 -0600

    Ok.

--- python/c2m2_schema_utils.py
+++ python/c2m2_schema_utils.py
@@ -5,26 +5,41 @@ import json
 import pandas as pd
 
 #############################################################################
-def Schema2TsvHeader(fin, table, fout):
-  tags=None;
+def LoadSchema(fin):
   schema = json.load(fin)
   logging.debug(json.dumps(schema, indent=2)+"\n")
+  return schema
+
+#############################################################################
+def Schema2TableHeader(schema, table, fout):
+  logging.info(f"Table: {table}")
+  tags=None;
   for resource in schema["resources"]:
     if resource["name"]==table:
       fields = resource["schema"]["fields"]
       tags = [field["name"] for field in fields]
       break
   if tags is None:
-    logging.error(f"Schema not found for table: {table}")
+    logging.error(f"Schema header not found for table: {table}")
   else:
     df = pd.DataFrame({tag:[] for tag in tags})
     df.to_csv(fout, "\t", index=False)
 
 #############################################################################
+def Schema2ListTables(schema, fout):
+  tables = [resource["name"] for resource in schema["resources"]]
+  tables.sort()
+  for table in tables:
+    fout.write(f"{table}\n")
+  return tables
+
+#############################################################################
 if __name__=="__main__":
-  parser = argparse.ArgumentParser(description='C2M2 schema JSON to TSV header converter')
+  OPS=["tableheader", "list_tables", ]
+  parser = argparse.ArgumentParser(description='C2M2 schema utilities')
+  parser.add_argument("op", choices=OPS, help="OPERATION")
   parser.add_argument("--i", dest="ifile", required=True, help="input JSON schema file (C2M2_datapackage.json)")
-  parser.add_argument("--table", required=True, help="table name")
+  parser.add_argument("--table", default="file", help="table name")
   parser.add_argument("--o", dest="ofile", help="output (TSV)")
   parser.add_argument("-v", "--verbose", action="count", default=0)
   args = parser.parse_args()
@@ -32,7 +47,16 @@ if __name__=="__main__":
   logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
 
   fin = open(args.ifile)
-
   fout = open(args.ofile, "w+") if args.ofile else sys.stdout
 
-  Schema2TsvHeader(fin, args.table, fout)
+  if args.op=="tableheader":
+    schema = LoadSchema(fin)
+    Schema2TableHeader(schema, args.table, fout)
+
+  elif args.op=="list_tables":
+    schema = LoadSchema(fin)
+    Schema2ListTables(schema, fout)
+
+  else:
+    parser.error(f"Unsupported operation: {args.op}")
+
